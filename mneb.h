bac27b2be6a8e1af2621ad0a5732e76e222d7cd7
Swift <swiftshineexists@gmail.com>
2026-02-01 00:14:32 -0600

    update mneb header

--- mneb.h
+++ mneb.h
@@ -1,62 +1,86 @@
-// not necessarily a valid C/++ representation but it gets the point across
-
 #pragma once
 
 #include <cstdint>
 
-#define UNK_SIZE 1
+// used to indicate that an array is variable-length
+#define UNDEFINED_SIZE 1
 
-template <typename T>
-struct Array {
-    uint32_t mCount;
-    T mItems[/* mCount */];
-};
+// all offsets are relative to the start of the file.
+using Offset = uint32_t;
 
-struct ControlPoint {
-    /* 0x00 */ int16_t x;
-    /* 0x02 */ int16_t y;
-    /* 0x04 */ int16_t z;
-    /* 0x06 */ int16_t w; // weight
-};
-
-// variable size
-struct CurveBlock {
-    /* 0x00 */ uint32_t m_0;
-    /* 0x04 */ uint32_t m_4;
-    /* 0x08 */ uint32_t m_8;
-    /* 0x0C */ uint32_t mControlPointOffset;
-    /* 0x10 */ uint32_t mKnotOffset;
-    /* 0x14 */ uint32_t m_14; // some offset
-    /* 0x18 */ float m_18[8]; // transforms?
-    /* 0x38 */ Array<ControlPoint> mControlPoints;
-    /* 0x?? */ Array<float> mKnots;
-    /* 0x?? */ uint32_t mUnk1; // the offset to mUnkArray2. i.e. &mUnkArray2 == (&mUnk1 + mUnk1)
-    /* 0x?? */ char mUnkData1[UNK_SIZE]; // maybe it's animation data?
-    /* 0x?? */ Array<uint32_t> mUnkArray2;
-};
-
-// variable size due to ^
-struct CurveEntry {
-    /* 0x00 */ char mMagic[4]; // "MNCN"
-    /* 0x04 */ uint32_t mCurveSize; // the size of this instance of the structure
-    /* 0x08 */ char mName[0x20];
-    /* 0x28 */ char m_28[0x68]; // maybe a float array? there's usually a float at the very end
-    /* 0x90 */ CurveBlock mCurveBlock;
-};
-
-// size: 0x18
+// Size: 0x18
 struct Header {
-    /* 0x00 */ char mMagic[4]; // "MNCH"
-    /* 0x04 */ uint32_t mCurveEntryOffset;
-    /* 0x08 */ uint32_t mFlags; // not sure what the flags are though
-    /* 0x0C */ uint32_t mCurveEntryCount;
+    /* 0x00 */ char mMagic[4];              // MNCH
+    /* 0x04 */ Offset mCurveEntryOffset;
+    /* 0x08 */ uint32_t m_8;                // flags? not sure.
+    /* 0x0C */ uint32_t mNumCurveEntries;
     /* 0x10 */ uint32_t m_10;
     /* 0x14 */ int16_t m_14;
     /* 0x16 */ bool m_16;
 };
 
-struct MNEBFile {
-    /* 0x00 */ Header mHeader;
-    /* 0x18 */ CurveEntry mCurveEntries[/* mHeader.mCurveEntryCount */];
+/* Demo */
+
+struct DemoOption {
+    /* 0x00 */ char mName[0x10];
+    /* 0x10 */ uint32_t mNumUsedValues;
+    /* 0x14 */ uint8_t mValues[/* mNumUsedValues */ UNDEFINED_SIZE];
 };
 
+struct DemoOptionSet {
+    /* 0x00 */ char mName[0x20];
+    /* 0x20 */ char m_20[0x20];
+    /* 0x40 */ uint32_t mNumOptions;
+    /* 0x44 */ Offset mDemoOptionOffsets[/* mNumOptions */ UNDEFINED_SIZE];
+};
+
+struct DemoDataBlock {
+    /* 0x0 */ char mMagic[4]; // MNDD
+    /* 0x4 */ uint32_t mBlockSize;
+    /* 0x8 */ uint32_t mNumDemoOptionSets;
+    /* 0xC */ Offset mDemoOptionSetOffsets[/* mNumDemoOptionSets */ UNDEFINED_SIZE];
+};
+
+/* Curve */
+
+// Size: 0x8
+struct ControlPoint {
+    /* 0x0 */ int16_t x;
+    /* 0x2 */ int16_t y;
+    /* 0x4 */ int16_t z;
+    /* 0x6 */ int16_t w;
+};
+
+// Size: 0x8
+struct KeyFrame {
+    int16_t frame;
+    bool active;
+    /* padding of 1*/
+    int16_t x;
+    int16_t y;
+};
+
+struct CurveBlock {
+    /* 0x00 */ char mMagic[4]; // MNCN
+    /* 0x04 */ uint32_t mBlockSize;
+    /* 0x08 */ char mName[0x20];
+    /* 0x28 */ uint8_t m_28[0x64];
+    /* 0x8C */ float m_8C;
+    /* 0x90 */ uint32_t m_90;
+    /* 0x94 */ uint32_t m_94;
+    /* 0x98 */ uint32_t m_98;
+    /* 0x9C */ Offset mControlPointOffset;
+    /* 0xA0 */ Offset mKnotOffset;
+    /* 0xA4 */ Offset mAnimationDataOffset;
+    /* 0xA8 */ float m_A8[4];
+    /* 0xB8 */ uint32_t m_B8[4];
+    /* 0xC8 */ uint32_t mNumControlPoints;
+    /* 0xCC */ ControlPoint mControlPoints[/* mNumControlPoints */ UNDEFINED_SIZE];
+    /* ---- */ uint32_t mNumKnots;
+    /* ---- */ float mKnots[/* mNumKnots */ UNDEFINED_SIZE];
+    /* ---- */ Offset mTransformTableOffset;
+    /* ---- */ uint8_t padding[0xC]; // explicit padding
+    /* ---- */ KeyFrame mKeyFrames[UNDEFINED_SIZE];
+    /* ---- */ uint32_t mNumKeyFrames;
+    /* ---- */ Offset mKeyFrameOffsets[/* mNumKeyFrames */ UNDEFINED_SIZE];
+};
